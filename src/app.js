0cc25b43dd72915cacceef067429ab3ecfbbd676
omarvegaabreu <omarvega@hotmail.com>
2020-03-15 16:04:53 -0400

    first commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+"use strict";
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "normalize.css";
+import "./styles/styles.scss";
+
+ReactDOM.render(<p>boiler plate</p>, document.getElementById("app"));
