3e2d43908b4df234e246027351c07bad00618628
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2020-03-23 10:20:44 -0400

    higher order components

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/app.js",
+  entry: "./src/Playground/hoc.js",
   output: {
     path: path.join(__dirname, "public"),
     filename: "bundle.js"
