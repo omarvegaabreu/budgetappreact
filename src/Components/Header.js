6c301a0f6b3090311dd40bcda17f4856bc912082
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2020-03-24 19:48:00 -0400

    update files

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,22 @@
-import React from 'react'
+import React from "react";
 import { NavLink } from "react-router-dom";
 
 const Header = () => (
   <header>
     <h1>HEADER</h1>
-    <NavLink to="/" activeClassName="is-active" exact={true} >Dashboard</NavLink>
-    <NavLink to="/add-expense" activeClassName="is-active">Add Expense</NavLink>
-    <NavLink to="/edit" activeClassName="is-active">Edit</NavLink>
-    <NavLink to="/help" activeClassName="is-active">Help</NavLink>
-  </header >
+    <NavLink to="/" activeClassName="is-active" exact={true}>
+      Dashboard
+    </NavLink>
+    <NavLink to="/add-expense" activeClassName="is-active">
+      Add Expense
+    </NavLink>
+    <NavLink to="/edit" activeClassName="is-active">
+      Edit
+    </NavLink>
+    <NavLink to="/help" activeClassName="is-active">
+      Help
+    </NavLink>
+  </header>
+);
 
-)
-
-export default Header
+export default Header;
