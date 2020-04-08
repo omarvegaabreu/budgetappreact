60b28c90fbb7d13d5329f1136195cb9135c28d36
omarvegaabreu <omarvega@hotmail.com>
2020-04-07 20:39:59 -0400

    added test database and env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,14 +1,12 @@
 import * as firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCwSBckf063RnPR15BfpLdgqyxual9ZAh4",
-  authDomain: "budgetappreact.firebaseapp.com",
-  databaseURL: "https://budgetappreact.firebaseio.com",
-  projectId: "budgetappreact",
-  storageBucket: "budgetappreact.appspot.com",
-  messagingSenderId: "401763330967",
-  appId: "1:401763330967:web:71907894b11c702706b2d2",
-  measurementId: "G-6DT87D2B2E",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
